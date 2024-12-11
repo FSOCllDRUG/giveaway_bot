afd8f6eba956795c48bea9d2bf0f52294b99361e
Vlad <vlad.a.borshch@gmail.com>
2024-12-11 12:33:01 +0300

    Fixed error handling in utils.py:48

--- tools/utils.py
+++ tools/utils.py
@@ -47,7 +47,10 @@ async def convert_id(old_id: int) -> str:
 
 async def get_channel_hyperlink(session: AsyncSession, channel_id: int) -> str:
     chat = await channel_info(session=session, channel_id=channel_id)
-    return f"<a href='{chat.invite_link}'>{chat.title+' бот кикнут. Удалите канал/группу из бота'}</a>"
+    if chat.invite_link is None:
+        return f"<a href='{chat.invite_link}'>{chat.title+' бот кикнут. Удалите канал/группу из бота'}</a>"
+    else:
+        return f"<a href='{chat.invite_link}'>{chat.title}</a>"
 
 
 async def is_admin(user_id: int) -> bool:
