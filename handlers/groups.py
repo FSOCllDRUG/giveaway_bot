b7674a245ff2e44a4ec452164c94a3b74ca9418f
Vlad <vlad.a.borshch@gmail.com>
2024-12-30 02:19:50 +0300

    Removed handling of demoting bot

--- handlers/groups.py
+++ handlers/groups.py
@@ -18,12 +18,14 @@ async def on_chat_member_updated(update: ChatMemberUpdated):
         user_id = update.from_user.id
         await redis_temp_channel(user_id, chat_id)
         print(f"Bot promoted to admin in group/supergroup {chat_id} by user {user_id}")
-    if update.new_chat_member.status == 'member':
-        chat_id = update.chat.id
-        user_id = update.from_user.id
-        print(f"Bot demoted in group/supergroup {chat_id} by user {user_id}")
-        await not_admin(chat_id, user_id)
+    # if update.new_chat_member.status == 'member':
+    #     chat_id = update.chat.id
+    #     user_id = update.from_user.id
+    #     print(f"Bot demoted in group/supergroup {chat_id} by user {user_id}")
+    #     await not_admin(chat_id, user_id)
     if update.new_chat_member.status == 'left':
         chat_id = update.chat.id
         user_id = update.from_user.id
         print(f"Bot kicked from group/supergroup {chat_id} by user {user_id}")
+        await not_admin(chat_id, user_id)
+
