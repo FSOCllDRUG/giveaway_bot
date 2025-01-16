c2f191dae5914aa376e28ed966c2acb0ee17b324
Vlad <vlad.a.borshch@gmail.com>
2025-01-16 14:41:55 +0300

    refactor: Trying to enhance graph image

--- tools/graph.py
+++ tools/graph.py
@@ -1,14 +1,27 @@
-import matplotlib.pyplot as plt
 import io
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
 async def create_graph(data):
     dates, users = zip(*data)
-    plt.plot(dates, users)
+    fig, ax = plt.subplots()
+
+    # Поворот меток оси X
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.xticks(rotation=45)
+
+    # Добавление сетки
+    ax.grid(True)
+
+    # Построение графика
+    plt.plot(dates, users, marker='o', color='b')
     plt.xlabel('Дата регистрации')
     plt.ylabel('Количество новых пользователей')
     plt.title('Активность регистрации пользователей')
 
+    # Сохранение изображения в память
     img_data = io.BytesIO()
     plt.savefig(img_data, format='png')
     img_data.seek(0)
