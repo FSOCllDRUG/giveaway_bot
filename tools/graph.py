0759286bbfd231e7aeb710014ba257a08507ea74
Vlad <vlad.a.borshch@gmail.com>
2025-01-16 18:34:50 +0300

    refactor: edited graph

--- tools/graph.py
+++ tools/graph.py
@@ -1,26 +1,40 @@
-import io
-
-import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import io
 
 
 async def create_graph(data):
     months, user_counts = zip(*data)
     fig, ax = plt.subplots(figsize=(15, 8))
 
+    # Установка сетки за столбами
+    ax.set_axisbelow(True)
+    ax.grid(True, which='both', axis='both')
+
+    # Форматирование меток оси X
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     plt.xticks(rotation=90)
 
-    ax.grid(True)
+    # Определение цветовой палитры
+    colors = plt.cm.get_cmap('tab20', len(months)).colors
 
-    plt.bar(months, user_counts, color='b', width=20)
-    plt.xlabel('Месяц регистрации')
+    # Создание гистограммы с разными цветами
+    for i, (month, user_count) in enumerate(zip(months, user_counts)):
+        ax.bar(month, user_count, color=colors[i], width=20)
+
+    # Установка подписей оси X по центру под столбами
+    ax.set_xticks(months)
+    ax.set_xticklabels([month.strftime('%Y-%m') for month in months], rotation=90, ha='center')
+
+    plt.xlabel('Месяц')
     plt.ylabel('Количество новых пользователей')
     plt.title('Активность регистрации пользователей по месяцам')
 
+    # Добавление аннотаций
     for month, user_count in zip(months, user_counts):
-        ax.annotate(f'{user_count}', xy=(month, user_count), xytext=(5, user_count + 5), textcoords='offset points')
+        ax.annotate(f'{user_count}', xy=(month, user_count), xytext=(0, 5), textcoords='offset points', ha='center')
 
+    # Сохранение изображения в память
     img_data = io.BytesIO()
     plt.savefig(img_data, format='png')
     img_data.seek(0)
