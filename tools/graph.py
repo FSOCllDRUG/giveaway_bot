b6b0b2501592f97fe3c8dcae8ae1e4f2e1e8d47f
Vlad <vlad.a.borshch@gmail.com>
2025-01-16 14:45:33 +0300

    refactor: #2 Trying to enhance graph image

--- tools/graph.py
+++ tools/graph.py
@@ -1,14 +1,13 @@
-import io
-
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import io
 
 
 async def create_graph(data):
     dates, users = zip(*data)
     fig, ax = plt.subplots()
 
-    # Поворот меток оси X
+    # Поворот и форматирование меток оси X
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     plt.xticks(rotation=45)
 
@@ -16,10 +15,14 @@ async def create_graph(data):
     ax.grid(True)
 
     # Построение графика
-    plt.plot(dates, users, marker='o', color='b')
+    plt.plot(dates, users, marker='o', color='b', linestyle='-', linewidth=2, markersize=5)
     plt.xlabel('Дата регистрации')
     plt.ylabel('Количество новых пользователей')
     plt.title('Активность регистрации пользователей')
+
+    # Добавление аннотаций
+    for date, user_count in zip(dates, users):
+        ax.annotate(f'{user_count}', xy=(date, user_count), xytext=(5, 5), textcoords='offset points')
 
     # Сохранение изображения в память
     img_data = io.BytesIO()
