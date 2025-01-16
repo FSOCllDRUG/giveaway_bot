f5b8704464a83c579cbace045dba739cb76c61a9
Vlad <vlad.a.borshch@gmail.com>
2025-01-16 21:05:29 +0300

    refactor: Final version of graph.py

--- tools/graph.py
+++ tools/graph.py
@@ -1,14 +1,16 @@
 import io
+
 import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
 
 async def create_graph(data):
     months, user_counts = zip(*data)
     fig, ax = plt.subplots(figsize=(15, 8))
 
     # Сдвиг графика к правому краю и уменьшение отступов
-    plt.subplots_adjust(left=0.1, right=0.95, top=0.9, bottom=0.1)
+    plt.subplots_adjust(left=0.25, right=0.97, top=0.9, bottom=0.1)
 
     # Установка сетки за столбами
     ax.set_axisbelow(True)
@@ -40,14 +42,15 @@ async def create_graph(data):
 
     # Подсчет суммарного количества пользователей
     total_users = sum(user_counts)
-    legend_text = [f'{month.strftime("%Y-%m")}: {user_count} пользователей' for month, user_count in zip(months, user_counts)]
 
     # Создание отдельного объекта для общего количества пользователей
     total_patch = mpatches.Patch(color='none', label=f'Всего пользователей: {total_users}')
 
     # Добавление общего количества пользователей и легенды с месяцами в самый угол изображения
-    ax.legend(handles=[total_patch] + [mpatches.Patch(color=cmap(i / len(months)), label=f'{month.strftime("%Y-%m")}: {user_count}') for i, (month, user_count) in enumerate(zip(months, user_counts))],
-              loc='upper left', bbox_to_anchor=(-0.1, 1), fontsize=10, title='Статистика')
+    ax.legend(handles=[total_patch] + [
+        mpatches.Patch(color=cmap(i / len(months)), label=f'{month.strftime("%Y-%m")}: {user_count}') for
+        i, (month, user_count) in enumerate(zip(months, user_counts))],
+              loc='upper left', bbox_to_anchor=(-0.31, 1.1), fontsize=10, title='Статистика')
 
     # Сохранение изображения в память
     img_data = io.BytesIO()
