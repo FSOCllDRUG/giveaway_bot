b012818badbe5e0838450a7b0c4dff194e575984
Vlad <vlad.a.borshch@gmail.com>
2025-02-11 19:48:39 +0300

    fix: Added messages to log channel

--- handlers/channels.py
+++ handlers/channels.py
@@ -3,7 +3,8 @@ from aiogram.types import ChatMemberUpdated
 
 from db.r_operations import redis_temp_channel
 from filters.chat_type import ChatType
-from tools.utils import not_admin
+from tools.logs_channel import send_log
+from tools.utils import not_admin, get_user_creds, get_channel_hyperlink
 
 channel_router = Router()
 channel_router.my_chat_member.filter(ChatType("channel"))
@@ -16,8 +17,14 @@ async def on_chat_member_updated(update: ChatMemberUpdated):
         user_id = update.from_user.id
         await redis_temp_channel(user_id, chat_id)
         print(f"Bot promoted to admin in channel {chat_id} by user {user_id}")
+        await send_log(f"Бот стал админом в канале {await get_channel_hyperlink(chat_id)}\n"
+                       f"Пользователь {await get_user_creds(user_id)}"
+                       f"\n\n#права")
     if update.new_chat_member.status == 'left':
         chat_id = update.chat.id
         user_id = update.from_user.id
         print(f"Bot kicked from channel {chat_id} by user {user_id}")
+        await send_log(f"Бот кикнут из канала {chat_id}\n"
+                       f"Пользователь {await get_user_creds(user_id)}"
+                       f"\n\n#права")
         await not_admin(chat_id, user_id)
--- handlers/groups.py
+++ handlers/groups.py
@@ -3,7 +3,8 @@ from aiogram.types import ChatMemberUpdated
 
 from db.r_operations import redis_temp_channel
 from filters.chat_type import ChatType
-from tools.utils import not_admin
+from tools.logs_channel import send_log
+from tools.utils import not_admin, get_user_creds, get_channel_hyperlink
 
 group_router = Router()
 
@@ -18,6 +19,9 @@ async def on_chat_member_updated(update: ChatMemberUpdated):
         user_id = update.from_user.id
         await redis_temp_channel(user_id, chat_id)
         print(f"Bot promoted to admin in group/supergroup {chat_id} by user {user_id}")
+        await send_log(f"Бот стал админом в группе {await get_channel_hyperlink(chat_id)}\n"
+                       f"Пользователь {await get_user_creds(user_id)}"
+                       f"\n\n#права")
     # if update.new_chat_member.status == 'member':
     #     chat_id = update.chat.id
     #     user_id = update.from_user.id
@@ -27,5 +31,8 @@ async def on_chat_member_updated(update: ChatMemberUpdated):
         chat_id = update.chat.id
         user_id = update.from_user.id
         print(f"Bot kicked from group/supergroup {chat_id} by user {user_id}")
+        await send_log(f"Бот кикнут из группы {chat_id}\n"
+                       f"Пользователь {await get_user_creds(user_id)}"
+                       f"\n\n#права")
         await not_admin(chat_id, user_id)
 
